7959b5353c2a6d1eecb0440c94b163257889f899
Makae <makae90@gmail.com>
2015-06-07 09:12:00 +0200

    added config.php

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+define('STF2015_LOCATION_CONFIG', serialize(array(
+  'sportshall' => array(
+    'label' => __('Turnhalle', 'mgm-stf2015'),
+    'mapid' => '246',
+    'coords' => array(
+      'latitude' => '47.1441',
+      'longitude' => '7.357'
+    )
+  ),
+  'marathon' => array(
+    'label' => __('Start Cross-Lauf', 'mgm-stf2015'),
+    'mapid' => '245',
+    'coords' => array(
+      'latitude' => '47.147422',
+      'longitude' => '7.35878'
+    )
+  ),
+  'party' => array(
+    'label' => __('Festgelände', 'mgm-stf2015'),
+    'mapid' => '245',
+    'coords' => array(
+      'latitude' => '47.1441',
+      'longitude' => '7.357'
+    )
+  )
+)));
